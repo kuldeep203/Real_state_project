aff85e1d9e5f353015887a30410fb78f99aaa4d5
kuldeep <mechatronickuldeep@gmail.com>
2022-08-08 14:48:13 +0530

    first commit

---
+++ practice.py
@@ -0,0 +1,3 @@
+A = {'Name': 'Asif', 'ID': 12345, 'DOB': 1991, 'Job': 'Analyst', 1: 0}
+
+print(all(A))
---
+++ realtors/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Realtor,RealtorAdmin)
